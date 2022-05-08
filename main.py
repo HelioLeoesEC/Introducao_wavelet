1819db43d6c059caba36d28462fb01b814e8d697
Hélio Leões <88741779+HelioLeoesEC@users.noreply.github.com>
2022-05-07 21:56:18 -0300

    Final do dia de 07/05

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 def coeficiente(amostragem,teta,jota):
     real = round(np.cos((2*np.pi*jota*teta)/amostragem),2)
-    imaginario = round(np.sin((2*np.pi*jota*teta)/amostragem),2)
+    imaginario = (-1)*round(np.sin((2*np.pi*jota*teta)/amostragem),2)
     return [real, imaginario]
 def DFT(amostragem,x,d):
     if len(x) == len(d):
@@ -13,7 +13,16 @@ def DFT(amostragem,x,d):
             for j in range(len(x)):
                 retorno = coeficiente(amostragem=amostragem,teta=teta,jota=j)
                 real = real + d[j]* retorno[0]
-                imaginaria = real + d[j]*retorno[1]
-            d_chapeu.append([real,imaginaria])
+                imaginaria = imaginaria + d[j]*retorno[1]
+            d_chapeu.append([round(real,2),round(imaginaria,2)])
+        magnitude = []
+        fase = []
+        for i in d_chapeu:
+            magnitude.append(round(np.sqrt(i[0]**2 + i[1]**2),2))
+        #Plotagem dos valores 
     else:
         print("As matrizes não são iguais")
+x = np.arange(0,np.pi,0.3)
+d = 1 + np.sin(x)
+
+DFT(amostragem=len(x),x=x,d=d)
